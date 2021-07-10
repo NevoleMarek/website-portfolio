c0dd7a2a0402ed005a8d042ed8406165759f18a7
NevoleMarek <nevole.marek@gmail.com>
2021-07-10 17:13:56 +0200

    Add first final version of home landing page

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -1,7 +1,5 @@
-console.log('asd');
 window.addEventListener('scroll',() => {
     let navbar = document.querySelector('nav');
-    console.log(window.scrollY);
     if (window.scrollY === 0){
         navbar.classList.remove('nav-scrolled');
     }
@@ -9,5 +7,3 @@ window.addEventListener('scroll',() => {
         navbar.classList.add('nav-scrolled');
     }
 })
-
-
