d43bd58e0269e48b00d9a375037a987cf1cabcb4
Marek Nevole <nevole.marek@gmail.com>
2021-07-11 12:43:41 +0200

    Add thumbnail to model

--- website/articles/models.py
+++ website/articles/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.urls import reverse
+
+
 # Create your models here.
 
 class Article(models.Model):
     title = models.CharField(max_length=50)
     created = models.DateField(auto_now_add=True)
     modified = models.DateField(auto_now=True)
+    thumbnail = models.ImageField()
     content = models.TextField()
